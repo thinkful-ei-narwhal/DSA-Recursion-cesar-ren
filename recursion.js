5485f16fe58e710dbdce96318c67318d14bd9fca
Ren F <renfinn89@gmail.com>
2020-06-08 09:19:42 -0700

    second problem

--- recursion.js
+++ recursion.js
@@ -1,14 +1,24 @@
 'use strict';
 
-const countSheep= function(sheep) {
-  if(sheep===0){
-    return  console.log('All sheep jumped over the fence');
+const countSheep = function (sheep) {
+  if (sheep === 0) {
+    return console.log('All sheep jumped over the fence');
   }
 
   console.log(sheep + ' Another sheep jumps over the fence');
-  countSheep(sheep-1);
+  countSheep(sheep - 1);
 };
 
 countSheep(3);
 
+const powerCalculator = function (base, exponent) {
+  if (exponent < 0) {
+    return 'exponent should >= 0';
+  }
+  if (exponent === 0) {
+    return 1;
+  }
+  return base * powerCalculator(base, exponent - 1);
+};
 
+console.log(powerCalculator(10, 2));
