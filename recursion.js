b497fb685e8e5c42042ed1c05fd4398c0f71bdfe
Cesar Morales <cesarlenin2@live.com>
2020-06-08 09:42:26 -0700

    question 3 done

--- recursion.js
+++ recursion.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//1. Counting Sheep
 const countSheep = function (sheep) {
   if (sheep === 0) {
     return console.log('All sheep jumped over the fence');
@@ -11,6 +12,7 @@ const countSheep = function (sheep) {
 
 countSheep(3);
 
+//2. Power Calculator
 const powerCalculator = function (base, exponent) {
   if (exponent < 0) {
     return 'exponent should >= 0';
@@ -22,3 +24,20 @@ const powerCalculator = function (base, exponent) {
 };
 
 console.log(powerCalculator(10, 2));
+
+//3. Reverse String
+const reverseString= function(word) {
+  if(word===''){
+    return  '';
+  }
+  return word.charAt(word.length-1)+ reverseString(word.slice(0,-1));
+};
+
+console.log(reverseString('the world'));
+
+//4. nth Triangular Number
+const tringularNumber = function (base, exponent) {
+
+};
+  
+console.log(tringularNumber());
